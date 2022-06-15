7d4bf08f24ecffd4317db9db3d2391689d6fb542
luis.g.vasconcelos <c0490417@vale.com>
2022-06-15 16:34:37 -0300

    config(#001): first commit!

---
+++ src/main/java/com/as/takehomeassessement/TakeHomeAssessementApplication.java
@@ -0,0 +1,13 @@
+package com.as.takehomeassessement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHomeAssessementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeHomeAssessementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/as/takehomeassessement/TakeHomeAssessementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.takehomeassessement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeAssessementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
